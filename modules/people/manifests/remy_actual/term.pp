e46e100059f2f4251c747758eb59acd90087fb69
remy-actual <remy@rootandflow.com>
2016-04-20 09:14:33 -0700

    loads iterm2 prefs from user directory

--- modules/people/manifests/remy_actual/term.pp
+++ modules/people/manifests/remy_actual/term.pp
@@ -2,11 +2,31 @@ class people::remy_actual::term {
 
   notify { 'class people::remy_actual::term declared': }
 
-  # Install and configure iterm2
-
+  ## iTerm2
+  # install
   include iterm2::stable
+
+  # color schemes
   include iterm2::colors::solarized_light
+  include iterm2::colors::solarized_dark
 
+  # set preferences from file
 
+  $prefs_path = "/Users/${::boxen_user}/dotfiles/term"
+
+  boxen::osx_defaults { 'Enable iterm preference loading':
+    user   => $::boxen_user,
+    domain => 'com.googlecode.iterm2',
+    key    => 'LoadPrefsFromCustomFolder',
+    value  => 1,
+    type   => 'integer',
+  } ->
+  boxen::osx_defaults { 'Set iterm preference path':
+    user   => $::boxen_user,
+    domain => 'com.googlecode.iterm2',
+    key    => 'PrefsCustomFolder',
+    value  => $prefs_path,
+    type   => 'string'
+  }
 
 }
